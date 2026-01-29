0400ff2c00848698d3e7c37597b73b91f893bde2
Andrey Chichak <andrey@initech.co.il>
2026-01-29 13:40:49 +0300

    initial

---
+++ scrap.sh
@@ -0,0 +1,2 @@
+npx @mintlify/scraping@latest openapi-file ./openapi/public_api_spec.yaml  -o public-api-reference
+npx @mintlify/scraping@latest openapi-file ./openapi/partner_api_spec.yaml  -o partner-api-reference
