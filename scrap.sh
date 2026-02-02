d2c21ff07a394ae25a361ec2eb41d7a06faa83a2
Andrey Chichak <andrey@initech.co.il>
2026-02-02 13:38:28 +0300

    DEV-4481 - adopted and added all YAML schemas.

--- scrap.sh
+++ scrap.sh
@@ -1,2 +1,4 @@
 npx @mintlify/scraping@latest openapi-file ./openapi/public_api_spec.yaml  -o public-api-reference
+npx @mintlify/scraping@latest openapi-file ./openapi/webhook_spec.yaml  -o webhook-reference
 npx @mintlify/scraping@latest openapi-file ./openapi/partner_api_spec.yaml  -o partner-api-reference
+npx @mintlify/scraping@latest openapi-file ./openapi/partner_api_webhook_spec.yaml  -o partner-api-webhook-reference
